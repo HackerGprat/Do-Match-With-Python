d5900e7066c439dd41efb3f0b60f8cf7f71f5b92
HackerGprat <41012110+HackerGprat@users.noreply.github.com>
2021-10-02 12:49:18 +0530

    first readme

---
+++ readme.md
@@ -0,0 +1,4 @@
+
+# Do Maths With Python
+
+In this repo bla bla bla
---
+++ startup.py
@@ -0,0 +1,8 @@
+def tables(number):
+
+    for i in range(1,11):
+        print( number * i)
+            
+
+if __name__ == "__main__":
+    tables(2)
